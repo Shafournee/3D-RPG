5aa79ff65a5877e4b4fdcf78f063ee8a0bde35db
Shane Lesser <shafournee@gmail.com>
2020-03-01 14:35:30 -0500

    First Commit

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.transform.position);
+    }
+}
---
+++ Assets/Scripts/ControlPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPlayer : MonoBehaviour
+{
+    float speed, rotatioAroundY;
+    Animator anim;
+    CharacterController controller;
+    AnimatorStateInfo info;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        info = anim.GetCurrentAnimatorStateInfo(0);
+        speed = Input.GetAxis("Vertical");
+        rotatioAroundY = Input.GetAxis("Horizontal");
+        anim.SetFloat("speed", speed);
+        gameObject.transform.Rotate(0, rotatioAroundY, 0);
+        if (speed > 0) controller.Move(transform.forward * speed * 2.0f * Time.deltaTime);
+    }
+}
